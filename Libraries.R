b28e9d3f3bbcd2f4f7f83ef7a614443d380c5dff
Mitsuo Shiota <mitsuo.shiota.xv@hitachi.com>
2019-03-19 19:39:55 +0900

    test via RStudio

---
+++ Libraries.R
@@ -0,0 +1,65 @@
+# libraries
+library(tidyquant)
+
+# self-made tq functions
+tq_diff <- function(df, n = 1) {
+  df %>% 
+    group_by(symbol) %>% 
+    mutate(
+      price_lag = lag(price, n),
+      price = price - price_lag
+    ) %>% 
+    ungroup() %>% 
+    select(-price_lag)
+}
+
+tq_ma <- function(df, n = 3) {
+  df %>% 
+    group_by(symbol) %>% 
+    tidyquant::tq_mutate(
+      select = price,
+      mutate_fun = SMA,
+      n = n
+    ) %>% 
+    ungroup() %>% 
+    rename(
+      foo = price,
+      price = SMA
+    ) %>% 
+    select(-foo)
+}
+
+tq_gr <- function(df, n = 12, annualize = 1) {
+  df %>% 
+    group_by(symbol) %>% 
+    mutate(
+      price_lag = lag(price, n),
+      price = (price / price_lag)^annualize * 100 - 100
+    ) %>% 
+    ungroup() %>% 
+    select(-price_lag)
+}
+
+price_sa <- function(df, frequency) {
+  df %>%   
+    mutate(
+      price = price %>% 
+        ts(start = c(lubridate::year(date[1]), lubridate::month(date[1])),
+           frequency = frequency),
+      price = price %>% 
+        seasonal::seas() %>% 
+        seasonal::final() %>% 
+        as.numeric()
+    )
+}
+
+tq_sa <- function(df, frequency = 12) {
+  df %>% 
+    group_by(symbol) %>% 
+    nest() %>% 
+    mutate(
+      data = map(data, price_sa, frequency = frequency)
+    ) %>% 
+    unnest(data) %>% 
+    ungroup()
+}
---
+++ Yellen_labor_mkt.R
@@ -0,0 +1,113 @@
+# First execute Libraries.R
+# for tidyverse, other libraries, and self-made utility functions
+
+# Yellen's labor market dashboard
+# note: https://www.bloomberg.com/graphics/2015-yellens-labor-market-dashboard/
+# symbols in FRED
+yellen_labor_mkt_symbols <- c(
+  "UNRATE", # Unemployment rate
+  "U6RATE", # U-6, underemployment rate
+  "LNS13025703", # Long-term unemployed share
+  "PAYEMS", # Non-farm payrolls, total
+  "JTSJOR", # Job openings rate
+  "JTSLDR", # Layoffs/discharged rate
+  "JTSQUR", # Quits rate
+  "JTSHIR", # Hires rate
+  "CEU0500000003", # Average hourly earnings, total private
+  "ECIALLCIV", # Employment cost index, all civilians, quaterly
+  "CIVPART", # Labor force participation rate
+  "PCEPILFE" # PCE excluding food and energy
+  ) 
+
+# get data from this date
+START = "2006-01-01"
+
+# get data from FRED
+labor_mkt_m <- yellen_labor_mkt_symbols %>% 
+  tidyquant::tq_get(get = "economic.data", from = START)
+
+# transform Non-farm payrolls, total to differences, 3 month moving average
+payems <- labor_mkt_m %>% 
+  filter(symbol == "PAYEMS") %>% 
+  tq_diff() %>% 
+  tq_ma(n = 3)
+
+# transform PCE to growth rates, YoY
+pce <- labor_mkt_m %>% 
+  filter(symbol == "PCEPILFE") %>% 
+  tq_gr(n = 12)
+
+# transform Average hourly earnings to YoY, 3 ma
+ceu <- labor_mkt_m %>% 
+  filter(symbol == "CEU0500000003") %>% 
+  tq_gr(n = 12) %>% 
+  tq_ma(n = 3)
+
+# transform Employment cost index, quarterly data to YoY
+eci <- labor_mkt_m %>% 
+  filter(symbol == "ECIALLCIV") %>% 
+  tq_gr(n = 4)
+
+# Atlanta Fed Wage Trucker
+# source: https://www.frbatlanta.org/chcs/wage-growth-tracker.aspx
+wage_tracker <- read_csv("data/wage-growth-data.csv", na = c(".", ""),
+                         col_types = cols(X1 = col_date(format = "%Y/%m/%d"),
+                                          `Unweighted Overall` = col_double()))
+names(wage_tracker) <- c("date", "price")
+wage_tracker$symbol <- "WAGETR"
+wage_tracker <- wage_tracker %>% 
+  filter(date >= as.Date(START))
+
+# combine transformed data, and change symbol order
+labor_mkt <- labor_mkt_m %>% 
+  filter(!(symbol %in% c("PAYEMS", "PCEPILFE", "CEU0500000003", "ECIALLCIV"))) %>% 
+  bind_rows(payems) %>% 
+  bind_rows(pce) %>% 
+  bind_rows(ceu) %>% 
+  bind_rows(eci) %>% 
+  bind_rows(wage_tracker) %>% 
+  mutate(symbol = fct_relevel(symbol,
+                              "UNRATE", "LNS13025703", "U6RATE", "CIVPART",
+                              "JTSJOR", "JTSLDR", "JTSQUR", "JTSHIR",
+                              "PAYEMS", "CEU0500000003", "ECIALLCIV", "WAGETR",
+                              "PCEPILFE"),
+         symbol = fct_recode(symbol,
+                             "Unemployment rate" = "UNRATE",
+                             "Long-term unemployed share" = "LNS13025703",
+                             "U-6 underemployment rate" = "U6RATE",
+                             "Labor force participation rate" = "CIVPART",
+                             "Job openings rate" = "JTSJOR",
+                             "Layoffs/discharged rate" = "JTSLDR",
+                             "Quits rate" = "JTSQUR",
+                             "Hires rate" = "JTSHIR",
+                             "Non-farm payroll increase, 3 mo avg" = "PAYEMS",
+                             "Avg hourly earnings, YoY, 3 mo avg" = "CEU0500000003",
+                             "Employment cost index, YoY, quarterly" = "ECIALLCIV",
+                             "Atlanta Fed wage tracker, YoY, 3 mo avg" = "WAGETR",
+                             "PCE excluding food and energy, YoY" = "PCEPILFE"
+                             )
+           )
+
+# plot with label only on the latest data
+labor_mkt %>% 
+  group_by(symbol) %>% 
+  mutate(
+    label = if_else(date == max(date), 
+                    paste(as.character(date), as.character(round(price, 2))
+                          , sep = "\n"), NA_character_)
+  ) %>% 
+  ungroup() %>% 
+  ggplot(aes(x = date, y = price)) + 
+  geom_line() +
+  facet_wrap(~ symbol, ncol = 4, scales = "free_y") +
+  geom_text(aes(label = label), na.rm = TRUE,
+            hjust = 1, vjust = 0) +
+  coord_cartesian(xlim = c(as.Date("2008-01-01"), as.Date("2019-01-01"))) +
+  labs(
+    title = "Yellen's labor market dashboard",
+    x = "",
+    y = "")
+
+# save pdf in (nearly) A4 format wide
+ggsave(filename = "output/Yellen.pdf",
+       width = 10, height = 8, units = "in", dpi = 300)
